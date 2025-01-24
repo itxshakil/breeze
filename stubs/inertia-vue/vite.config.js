438ffbdc3f0b1b33d474b031bfcbd6a13048eee6
Shakil Alam <itxshakil@gmail.com>
2025-01-24 12:20:57 +0000

    Reapply "fix: Remove TailwindCSS plugin from Vite configuration"
    This reverts commit 7a61e00fe10baf9f1aabb44f14f7ffefa4050e4b.

--- stubs/inertia-vue/vite.config.js
+++ stubs/inertia-vue/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -16,7 +15,6 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
-        }),
-        tailwindcss(),
+        })
     ],
 });
