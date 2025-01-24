a1c665a1fb342b80572eb9339ac5a668c92dd7dc
itxshakil <30234430+itxshakil@users.noreply.github.com>
2025-01-24 15:39:30 +0000

    ESLint: fix code styling

--- stubs/inertia-react-ts/resources/js/Components/Checkbox.tsx
+++ stubs/inertia-react-ts/resources/js/Components/Checkbox.tsx
@@ -9,7 +9,7 @@ export default function Checkbox({
             {...props}
             type="checkbox"
             className={
-                'rounded-sm border-gray-300 text-indigo-600 shadow-xs focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 ' +
+                'shadow-xs rounded-sm border-gray-300 text-indigo-600 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 ' +
                 className
             }
         />
